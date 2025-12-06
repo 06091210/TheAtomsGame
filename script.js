5a980be5ec5d61fa87596cffa327d9da062366b3
06091210 <firp771@gmail.com>
2025-12-06 17:23:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ space = 103
 line = Math. floor (window.innerWidth / space)
 row = Math. floor (window.innerHeight / space)
 shiftX = (window.innerWidth - (space * line)) / 2
-shiftY = 0
+shiftY = 10
 elements = ["H", "O"]
 colors = ["#00A0F0", "#0000C0"]
 Scolors = []
@@ -15,7 +15,7 @@ for (let i=0; i<line*row; i++) {
   Scolors.push(colors[random]);
 }
 for (let j=0; j<selects.length; j++) {
-  document. write(`<h1 class='circle' style="background: ${Scolors[j]}; translate: ${shiftX}px -30px" id=${j}>${selects[j]}</h1>`);
+  document. write(`<h1 class='circle' style="background: ${Scolors[j]}; translate: ${shiftX}px -20px" id=${j}>${selects[j]}</h1>`);
 }
 window.addEventListener('touchstart', (e) => {
   table = [["H", "H", "O"], ["O", "O"], ["O", "O", "O"], ["H", "H"], ["O", "O", "H", "H"]]
